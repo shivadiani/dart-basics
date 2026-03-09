f5201d5016a8bf4fb83b99a0b5ff2cb529057e74
shivaaaas <shivadianiputri01@gmail.com>
2026-03-09 12:06:14 +0700

    tipe data object

---
+++ dart_project/bin/object.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Object adalah induk dari semua tipe data di Dart
+  Object data = 'Halo Shiva!';
+  print('Isi Object awal (teks): $data');
+
+  // Karena Object adalah induk, kita bisa mengisinya dengan tipe lain
+  data = 100;
+  print('Isi Object diubah (angka): $data');
+}
