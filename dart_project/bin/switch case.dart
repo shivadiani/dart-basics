9526ec65da200f18abd7f91fdc5a8bbf62cbe226
shivaaaas <shivadianiputri01@gmail.com>
2026-03-09 12:52:57 +0700

    switch case

---
+++ dart_project/bin/switch case.dart
@@ -0,0 +1,13 @@
+void main() {
+  int nilai = 1;
+
+  // Memilih tindakan berdasarkan nilai
+  switch (nilai) {
+    case 1:
+      print('Satu');
+    case 2:
+      print('Dua');
+    default:
+      print('Angka lain');
+  }
+}
