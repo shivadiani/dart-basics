e77e2dd7b34434ba8444bfd51e4ab21991a97ae5
shivaaaas <shivadianiputri01@gmail.com>
2026-03-09 12:23:02 +0700

    operator aritmatika

---
+++ dart_project/bin/operator aritmatika.dart
@@ -0,0 +1,11 @@
+void main() {
+  int c = 6;
+  
+  // Menambahkan 6 ke dalam variabel c
+  c = c + 6; // c sekarang adalah 12
+  print('Nilai c sekarang: $c');
+
+  // Cara yang lebih singkat (Shorthand operator)
+  c += 6; // Sama fungsinya dengan c = c + 6;
+  print('Nilai c setelah ditambah 6 lagi: $c');
+}
