da6693b6bad7edb27469e2959cea0ce51174942e
shivaaaas <shivadianiputri01@gmail.com>
2026-03-09 12:34:57 +0700

    operator logika

---
+++ dart_project/bin/operator logika.dart
@@ -0,0 +1,9 @@
+void main() {
+  var A = true;
+  var B = false;
+  
+  // Operator Logika AND (&&): Akan bernilai true JIKA KEDUANYA true.
+  var hasil = A && B; 
+  
+  print('Apakah A dan B keduanya benar? $hasil'); // Output: false
+}
