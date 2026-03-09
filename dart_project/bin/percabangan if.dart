5f64c00faeb3202ddac1b79b6b8e4fd46f8b9222
shivaaaas <shivadianiputri01@gmail.com>
2026-03-09 12:45:53 +0700

    percabangan if

---
+++ dart_project/bin/percabangan if.dart
@@ -0,0 +1,9 @@
+void main() {
+  int nilaiLulus = 75;
+  int nilaiSiswa = 80;
+
+  // Jika kondisi di dalam kurung bernilai true, maka blok kode akan dijalankan
+  if (nilaiSiswa >= nilaiLulus) {
+    print('Selamat, kamu lulus!');
+  }
+}
