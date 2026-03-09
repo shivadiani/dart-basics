c192da5b9688dac4e4f43baa690ff1f0eccc0f9e
shivaaaas <shivadianiputri01@gmail.com>
2026-03-09 11:52:06 +0700

    tipe data boolean

---
+++ dart_project/bin/boolean.dart
@@ -0,0 +1,14 @@
+void main() {
+  bool isDartFun = true;
+  print('Apakah Dart menyenangkan? $isDartFun');
+
+  bool isTired = false;
+  print('Apakah kamu capek? $isTired');
+
+  // boolean sering digunakan untuk kondisi if-else
+  if (isDartFun) {
+    print('Ayo belajar Dart terus!');
+  } else {
+    print('Istirahat dulu ya.');
+  }
+}
