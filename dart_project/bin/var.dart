81967b331dda3e2deb657ceeecb7a887b79619b5
shivaaaas <shivadianiputri01@gmail.com>
2026-03-09 12:08:28 +0700

    tipe data var

---
+++ dart_project/bin/var.dart
@@ -0,0 +1,7 @@
+void main() {
+  // var menyuruh Dart untuk menebak tipe datanya sendiri.
+  // Karena isinya teks, maka 'penulis' otomatis menjadi String.
+  var penulis = 'Shiva';
+  
+  print('Nama penulis: $penulis');
+}
